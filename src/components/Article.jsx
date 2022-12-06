2d72802a59c9dc9111f17332319aab2cf44d234e
Dan <dan.phillips100@gmail.com>
2022-12-06 10:45:43 +0000

    Ticket 5: removed unused Link import

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getArticleById } from "../api";
 
 export default function Article() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -13,22 +14,25 @@ export default function Article() {
       setIsLoading(false);
     });
   }, [article_id]);
+
   return isLoading ? (
     <p className="loading-message">...loading</p>
   ) : (
-    <article className="single-article">
-      <p className="single-article--topic">{article.topic}</p>
-      <h2>{article.title}</h2>
-      <h3>By {article.author}</h3>
-      <div className="article-list--article-info--interactions">
-        <p>
-          <span>{article.votes}</span> Votes
-        </p>
-        <p>
-          <span>{article.comment_count}</span> Comments
-        </p>
-      </div>
-      <p className="single-article--body">{article.body}</p>
-    </article>
+    <>
+      <article className="single-article">
+        <p className="single-article--topic">{article.topic}</p>
+        <h2>{article.title}</h2>
+        <h3>By {article.author}</h3>
+        <div className="article-list--article-info--interactions">
+          <p>
+            <span>{article.votes}</span> Votes
+          </p>
+          <p>
+            <span>{article.comment_count}</span> Comments
+          </p>
+        </div>
+        <p className="single-article--body">{article.body}</p>
+      </article>
+    </>
   );
 }
